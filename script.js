f4bc3191dc806c94e3819405347f017b4149c3f2
Sarah Myriam <sarahmyriam@Sarahs-MacBook-Pro.local>
2020-09-09 22:50:23 -0400

    Just started the homework

---
+++ script.js
@@ -0,0 +1,6 @@
+var container = document.getElementsByClassName("container")[0]; 
+var timeBlock = document.createElement("div");
+//?append child
+container.appendChild(timeBlock);
+//adding a class to timeblocks
+var 
